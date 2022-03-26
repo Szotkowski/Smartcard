5e2630f93a7e8ad601e47e3833fc6a7448939dc0
Szotkowski <83296342+Szotkowski@users.noreply.github.com>
2022-03-26 21:22:14 +0100

    Add files via upload

---
+++ Smartcard/lib/main.dart
@@ -0,0 +1,40 @@
+  import 'dart:io';
+  import 'package:flutter/material.dart';
+  import 'package:splash_screen_view/SplashScreenView.dart';
+  
+  
+  
+  import 'src/pages/entry/page.dart';
+  ///NOTE:
+  ///if you have an error while running <flutter run> 
+  ///run <flutter pub upgrade> and than <flutter run --no-sound-null-safety>
+  void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+    
+    
+    
+    runApp(
+      MyApp()
+    );
+  }
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'first',
+        home: SplashScreenView(
+          navigateRoute: PageEntry(),
+          duration: 2200,
+          imageSize: 140,
+          imageSrc: 'assets/teta-app.png',
+          text: '',
+          textType: TextType.NormalText,
+          textStyle: TextStyle(
+            fontSize: 30.0,
+          ),
+          backgroundColor: Colors.black,
+        ),
+      );
+    }
+  }
+  
