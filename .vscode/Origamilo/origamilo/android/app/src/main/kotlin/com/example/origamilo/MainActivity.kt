b5b107d57f634696d0142c1e83903c58cf319739
Szotkowski <mikiszotkowski@gmail.com>
2022-09-30 21:10:09 +0200

    Origamilo

---
+++ .vscode/Origamilo/origamilo/android/app/src/main/kotlin/com/example/origamilo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.origamilo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ .vscode/Origamilo/origamilo/lib/src/pages/index.dart
@@ -0,0 +1,2 @@
+export './home/page.dart';
+export './entry/page.dart';
